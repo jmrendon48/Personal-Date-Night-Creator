0653e10457abd8e20af0e2bcdf90ed2d3b2341fb
Davit Shirkhanyan <davit.shirkhanyan@gmail.com>
2021-01-31 19:31:30 -0800

    update movie generator

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,7 +1,7 @@
-// add function to generate random movie
+// Movie generator
 $("#movie-btn").on("click", function findMovie() {
     userEntries();
-   
+    randomMovieGenerator();
   });
 
   // add function to test validity of user entries
@@ -15,7 +15,53 @@ $("#movie-btn").on("click", function findMovie() {
       $(".error").text("Please choose a year between 1900 and 2021.");
   } else if (parseInt(releaseYear) != releaseYear) {
       $(".error").text("Your year must be a four-digit number.");
+  } else {
+    $(".error").text("");
   }
+};
+
+// add function to generate a random movie
+let randomMovieGenerator = function() {
+    let releaseYear = $(".year-selector").val().trim();
+    let genre = $("#genres").val();
+    let apiKey = "183cf14b0fa970fabe87a2879d2f3aa1";
+
+    $.ajax( {
+        url: `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_date.gte=${releaseYear}-01-01&primary_release_date.lte=${releaseYear}-12-31&with_genres=${genre}&with_original_language=en`,
+        method: "GET",
+      }).then(function (response) {
+            let randomMovie = Math.floor(Math.random() * (response.results.length - 1));
+          for(let i = randomMovie; i < randomMovie + 1; i++) {
+              let fullYear = response.results[i].release_date;
+              let fourDigitYear = fullYear.substring(0, 4);
+              let title = response.results[i].original_title;
+              let parameters = $.param({
+                  t: title,
+                  y: fourDigitYear,
+                  plot: "short",
+                  apikey: "dadc91b3"
+              });
+
+              $.ajax({
+                  url: "https://www.omdbapi.com/?" + parameters,
+                  method: "GET",
+              }).then(function(response) {
+                  if(response.Response === "False") {
+                      findMovie();
+                  }
+                  $(".movie-poster").remove();
+                  $(".title").remove();
+                  $(".trailer").remove();
+                  let movieDiv = $("#movie-title");
+                  let movieTitle = $("<h3>").attr("class", "center-align title").html(response.Title);
+                  let movieImg = $("<img>").attr({ "src": response.Poster, "class": "movie-poster"});
+                  let movieTrailer = $("<a>").attr("href", `https://www.youtube.com/results?search_query=${response.Title}trailer`).attr("target", "_blank").attr("class", "btn primary-btn trailer").html("View trailer");
+                  let movieStream = $("<a>").attr("href", `https://www.justwatch.com/us/search?q=${response.Title}`).attr("target", "_blank");
+                  movieStream.append(movieImg);
+                  movieDiv.append(movieTitle, movieStream, movieTrailer);
+                });
+            }
+      });
 };
 
 
